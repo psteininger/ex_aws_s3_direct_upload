93c09800364d2801036af29e1079ceb44954fba4
Piotr Steininger <piotr.steininger@gmail.com>
2018-09-21 16:31:02 +0200

    Updated mix package details before publishing

--- mix.exs
+++ mix.exs
@@ -2,14 +2,17 @@ defmodule ExAws.S3.DirectUpload.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ex_aws_s3_direct_upload,
-     version: "1.0.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description(),
-     package: package(),
-     deps: deps()]
+    [
+      app: :ex_aws_s3_direct_upload,
+      version: "1.0.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      source_url: "https://github.com/laborvoices/ex_aws_s3_direct_upload",
+      description: description(),
+      package: package(),
+      deps: deps(),
+    ]
   end
 
   # Configuration for the OTP application
@@ -19,11 +22,13 @@ defmodule ExAws.S3.DirectUpload.Mixfile do
 
   # Dependencies
   defp deps do
-    [{:poison, "~> 2.0 or ~> 3.0"},
-     {:ex_aws_s3, "~> 2.0.1"},
-     {:hackney, "~> 1.9"},
-     {:sweet_xml, "~> 0.6"},
-     {:ex_doc, "~> 0.18", only: :dev, runtime: false}]
+    [
+      {:poison, "~> 2.0 or ~> 3.0"},
+      {:ex_aws_s3, "~> 2.0.1"},
+      {:hackney, "~> 1.9"},
+      {:sweet_xml, "~> 0.6"},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false}
+    ]
   end
 
   defp description do
@@ -34,14 +39,21 @@ defmodule ExAws.S3.DirectUpload.Mixfile do
 
   defp package do
     [
-     name: :ex_aws_s3_direct_upload,
-     files: ["lib", "mix.exs", "README*", "LICENSE*"],
-     maintainers: ["Piotr Steininger"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/laborvoices/ex_aws_s3_direct_upload",
-              "S3 Direct Uploads With Ember And Phoenix" => "http://haughtcodeworks.com/blog/software-development/s3-direct-uploads-with-ember-and-phoenix/",
-              "Browser-Based Upload using HTTP POST (Using AWS Signature Version 4)" => "http://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-post-example.html",
-              "Task 3: Calculate the Signature for AWS Signature Version 4" => "http://docs.aws.amazon.com/general/latest/gr/sigv4-calculate-signature.html"}
+      name: :ex_aws_s3_direct_upload,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      organization: "LaborVoices",
+      authors: ["Andrew Kappen", "Piotr Steininger"],
+      maintainers: ["Piotr Steininger"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/laborvoices/ex_aws_s3_direct_upload",
+        "S3 Direct Uploads With Ember And Phoenix" =>
+          "http://haughtcodeworks.com/blog/software-development/s3-direct-uploads-with-ember-and-phoenix/",
+        "Browser-Based Upload using HTTP POST (Using AWS Signature Version 4)" =>
+          "http://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-post-example.html",
+        "Task 3: Calculate the Signature for AWS Signature Version 4" =>
+          "http://docs.aws.amazon.com/general/latest/gr/sigv4-calculate-signature.html"
+      }
     ]
   end
 end
